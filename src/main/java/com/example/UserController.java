749a8d9295e7ff819376ace8008f035eaafcd09e
andrzej.zaniewicz <zaniewiczhg@gmail.com>
2024-03-20 17:34:04 +0100

    dodanie lombooka

--- src/main/java/com/example/UserController.java
+++ src/main/java/com/example/UserController.java
@@ -3,8 +3,11 @@ package com.example;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.reactivex.Observable;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 
 @Controller("/users")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
@@ -15,17 +18,19 @@ public class UserController {
 
     @Get
     public Observable<User> getUsers() {
+        log.info(" Start servcie");
         return userService.fetchUsers()
+                .doOnNext(user -> log.info(" Fetched user: " + user.getName()))
                 .onErrorReturn(throwable -> {
-            // Log the error
-            System.err.println("Error fetching users: " + throwable.getMessage());
+                    // Log the error
+                    log.error("Error fetching users: " + throwable.getMessage());
 
-            // Return an "empty" user to indicate an error
-            User errorUser = new User();
-            errorUser.setId("-1");
-            errorUser.setName("Error");
-            errorUser.setEmail("error@example.com");
-            return errorUser;
-        });
+                    // Return an "empty" user to indicate an error
+                    User errorUser = new User();
+                    errorUser.setId("-1");
+                    errorUser.setName("Error");
+                    errorUser.setEmail("error@example.com");
+                    return errorUser;
+                });
     }
 }
